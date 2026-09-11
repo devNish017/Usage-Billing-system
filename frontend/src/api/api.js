fa0c623601076e798d9726d88d50f8f9f63a2e8e
Nishant <nishantdwivedi017@gmail.com>
2026-09-11 19:53:05 +0530

    sixth commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://usage-billing-system.onrender.com/api"
 });
 
 export const createUser = async (userData) => {
